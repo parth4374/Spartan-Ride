04aadd8af898c141ba3077712a5f5b3a63c280c7
73MPL4R <nathanielwright34@yahoo.com>
2021-02-25 14:37:38 -0500

    Adding prototype map

---
+++ ProtoMap/app/src/main/java/com/example/protomap/MapsActivity.java
@@ -0,0 +1,50 @@
+package com.example.protomap;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng bike1 = new LatLng(36.069209, -79.809953);
+        LatLng bike2 = new LatLng(36.068012, -79.809804);
+        LatLng bike3 = new LatLng(36.065975, -79.811569);
+        mMap.addMarker(new MarkerOptions().position(bike1).title("Bike 1"));
+        mMap.addMarker(new MarkerOptions().position(bike2).title("Bike 2"));
+        mMap.addMarker(new MarkerOptions().position(bike3).title("Bike 3"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(bike1));
+    }
+}
