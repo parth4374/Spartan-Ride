de135b75871d3b4ea164a8aa4d8de74ab10df12e
73MPL4R <nathanielwright34@yahoo.com>
2021-03-18 15:15:24 -0400

    Added map boundaries and opening menu on click

--- ProtoMap/app/src/main/java/com/example/protomap/MapsActivity.java
+++ ProtoMap/app/src/main/java/com/example/protomap/MapsActivity.java
@@ -2,14 +2,20 @@ package com.example.protomap;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.telecom.Call;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.*;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -25,26 +31,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        LatLng campus = new LatLng(36.0689, 79.8102);
         LatLng bike1 = new LatLng(36.069209, -79.809953);
         LatLng bike2 = new LatLng(36.068012, -79.809804);
         LatLng bike3 = new LatLng(36.065975, -79.811569);
         mMap.addMarker(new MarkerOptions().position(bike1).title("Bike 1"));
         mMap.addMarker(new MarkerOptions().position(bike2).title("Bike 2"));
         mMap.addMarker(new MarkerOptions().position(bike3).title("Bike 3"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(bike1));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bike1, 16.0f));
+
+        // Create a LatLngBounds that includes the UNCG campus.
+        LatLngBounds campusBounds = new LatLngBounds(
+                new LatLng(36.064545, -79.815189), // SW bounds
+                new LatLng(36.072995, -79.804083)  // NE bounds
+        );
+
+        // Constrain the camera target to the UNCG bounds.
+        googleMap.setLatLngBoundsForCameraTarget(campusBounds);
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                String markerTitle = marker.getTitle();
+                Intent i = new Intent(MapsActivity.this, BikeDetails.class);
+                i.putExtra("title", markerTitle);
+                startActivity(i);
+
+                return false;
+            }
+        });
     }
 }
