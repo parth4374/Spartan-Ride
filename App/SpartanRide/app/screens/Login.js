65222207b29a4661439549a6b2737ee2bb82fbf8
arbraswe <arbraswe@gmail.com>
2021-04-08 17:16:45 -0400

    Update Login Screen

--- App/SpartanRide/app/screens/Login.js
+++ App/SpartanRide/app/screens/Login.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   View
 } from 'react-native'
 
@@ -26,27 +27,42 @@ export class Login extends React.Component {
         />
         <View style={styles.form}>
           <Text style={styles.textInputHeader}>UNCG Login:</Text>
-          <TextInput
-            placeholder="Email"
-            style={styles.textInput}
-            value={this.state.name}
-            onChangeText={(name) => this.setState({ name })}
-          />
-          <TextInput
-            placeholder="Password"
-            secureTextEntry={true}
-            style={styles.textInput}
-            value={this.state.password}
-            onChangeText={(password) => this.setState({ password })}
-          />
-          <Button
-            color="#666666"
-            title="Login"
-            onPress={() =>
-              navigation.navigate('Main', { name: this.state.name })
-            }
-            style={styles.button}
-          />
+          <View style={styles.inputContainer}>
+            <TextInput
+              placeholder="Email"
+              placeholderTextColor="#fff"
+              style={styles.textInput}
+              value={this.state.name}
+              underlineColorAndroid="#666666"
+              onChangeText={(name) => this.setState({ name })}
+            />
+            <Image
+              source={require('./arrowicon.png')}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor="#fff"
+              secureTextEntry={true}
+              style={styles.textInput}
+              value={this.state.password}
+              underlineColorAndroid="#666666"
+              onChangeText={(password) => this.setState({ password })}
+            />
+            <Image
+              source={require('./arrowicon.png')}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              color="#666666"
+              title="Login"
+              onPress={() =>
+                navigation.navigate('Main', { name: this.state.name })
+              }
+            />
+          </View>
         </View>
       </View>
     )
@@ -62,17 +78,29 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     aspectRatio: 0.5,
     flex: 1,
+    marginTop: -50,
+    marginBottom: -30,
     resizeMode: 'contain'
   },
+  inputContainer: {
+    alignItems: 'center',
+    flexDirection: 'row'
+    // flex: 1
+  },
   form: {
-    flex: 1
+    flex: 1,
+    paddingLeft: 20
   },
   textInputHeader: {
   },
   textInput: {
-    color: 'white'
+    color: 'white',
+    flex: 1,
+    textAlignVertical: 'top'
   },
   button: {
-    alignSelf: 'center'
+    alignSelf: 'center',
+    paddingTop: 10,
+    width: '80%'
   }
 })
